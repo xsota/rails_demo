e3d02ee327ccfc56319f42b7f78ec49e01b64d89
xsota <git@xsota.com>
2020-03-26 16:37:11 +0900

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'root/index'
   resources :animals
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'hello' => 'hello#index'
+
+  root to: 'root#index'
 end
